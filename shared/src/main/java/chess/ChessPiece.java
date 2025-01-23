af350e25b605b7ef58d76fa0f98ffe5b60c48569
SpencerK16 <46407049+SpencerK16@users.noreply.github.com>
2025-01-22 18:46:23 -0700

    Adding the rest of the classes

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -63,7 +63,7 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        ChessPieceMoveCalculator moves = switch (getPieceType()){
+        PieceMoveCalculator moves = switch (getPieceType()){
             case QUEEN -> new QueenMoveCalculator();
             case BISHOP  -> new BishopMoveCalculator();
             case KING -> new KingMoveCalculator();
