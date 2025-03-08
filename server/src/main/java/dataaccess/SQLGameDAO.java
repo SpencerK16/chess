e17dc876662a53ed057a9cb3abd79199349ba64a
SpencerK16 <46407049+SpencerK16@users.noreply.github.com>
2025-03-08 11:21:22 -0700

    Making the structre for the new game DAO

---
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -0,0 +1,31 @@
+package dataaccess;
+
+import model.GameData;
+import model.GameListData;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SQLGameDAO {
+
+    public SQLGameDAO() {
+    }
+
+    // Insert Game
+    public void insertGame(GameData gameData) throws DataAccessException {
+    }
+
+    // Get Game by ID
+    public GameData getGame(int gameID) throws DataAccessException {
+    }
+
+    public void deleteGame(int gameID) throws DataAccessException {
+    }
+
+    public void clear() throws DataAccessException {
+    }
+
+    public List<GameListData> getGames() throws DataAccessException {
+    }
+}
